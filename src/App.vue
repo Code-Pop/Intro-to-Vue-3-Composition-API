f5ceba0c06837289f65d538e07cdd7772bb67600
Paulius-U <paulius.ulevisius@gmail.com>
2025-08-19 23:35:03 +0300

    Add url and image to template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref } from 'vue'
+import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
+const image = ref(socksGreenImage)
+const url = ref('https://github.com/Paulius-U')
 
 </script>
 
@@ -9,10 +12,11 @@ const product = ref('Socks')
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <!-- image goes here -->
+        <img :src="image">
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+          <a :href="url">GitHub URL</a>
       </div>
     </div>
   </div>
